778cf61d8022b19637b34805d148b1ab19cb5966
Kevin Tromp <ketromp@gmail.com>
2014-10-26 09:57:45 -0500

    Problems 21 and 23 added

---
+++ Problem21.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem21
+    {
+        public int Amicable (int input)
+        {
+            int sum = 0, b = 0;
+            for (int a = 2; a < input; a++)
+            {
+                b = Divisors(a);
+                if(b > a)
+                    if(Divisors(b) == a)
+                        sum = sum + a + b;
+            }
+            return sum;
+        }
+
+        private int Divisors(int num)
+        {
+            int sum = -num;
+            for (int i = 1; i <= Math.Floor(Math.Sqrt(num)); i++)
+            {
+                if(num % i == 0)
+                {
+                    sum += i;
+                    sum += (num / i);
+
+                    if(i == (num / i))
+                        sum -= i;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Problem23.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem23
+    {
+        public int NonAbundantSum()
+        {
+            int sum = 0;
+            HashSet<int> abundants = new HashSet<int>();
+            HashSet<int> sumAbundants = new HashSet<int>();
+
+            for (int i = 12; i < 28123; i++)
+                if (isAbundant(i)) 
+                    abundants.Add(i);
+
+            foreach (int a in abundants)
+                foreach (int b in abundants)
+                    sumAbundants.Add(a + b);
+
+            for (int i = 1; i <= 28123; i++)
+                if (!sumAbundants.Contains(i))
+                    sum += i;
+
+            return sum;
+        }
+
+        private bool isAbundant(int num)
+        {
+            int sum = -num;
+            for (int i = 1; i <= Math.Floor(Math.Sqrt(num)); i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    sum += (num / i);
+
+                    if (i == (num / i))
+                        sum -= i;
+                }
+            }
+            return sum > num;
+        }
+    }
+}
